a59de04939280528e0c9afe10407c2ec7aa2b916
jreckin <76067343+jreckin@users.noreply.github.com>
2021-03-16 11:18:38 -0500

    Model

--- drill_selector.py
+++ drill_selector.py
@@ -10,5 +10,5 @@ from sklearn.model_selection import train_test_split
 from drill_selection import putts_inside_of_ten, up_and_down, round_entry, predict, importance, drill_suggestor, suggest_drills
 
 mock_round = round_entry(30, 18, 18, 5, 10, 30, 8, 8)
-
+print(importance(mock_round))
 suggest_drills(mock_round)
